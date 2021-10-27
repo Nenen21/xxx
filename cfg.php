61b480454f6443dcbf2e9d7a64df8a5a7c56f779
Nenen21 <93203336+Nenen21@users.noreply.github.com>
2021-10-28 02:53:12 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,5 @@
+<?php
+
+$useragent = "Mozilla/5.0 (Linux; Android 8.1.0; CPH1803) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.85 Mobile Safari/537.36";
+
+$cookie = "ref=54705,remember_code=82d65b4cbad0455142de13fef38d7c1407fce2e6.6fa5bafb587c7545b4a0bc72fa88d55e113db08b5a95f9af5ff42bd0093f86a10ead2c7fd4d7117e2cc024e6a7e3fdf5a8c73a8a0686685f57d5b45ed05df9ef";
